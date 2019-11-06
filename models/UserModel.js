cd23c51b56fdd4307e3dccf014bda54f1741a0f5
v.aleinik <v.aleinik@godeltech.com>
2019-11-06 12:29:19 +0300

    add user schema

---
+++ models/UserModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const { basic, supervisor, admin } = require('./constant');
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: basic,
+    enum: [basic, supervisor, admin],
+  },
+  accessToken: {
+    type: String,
+  },
+});
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
---
+++ models/constant.js
@@ -0,0 +1,7 @@
+const Roles = {
+  basic: 'basic',
+  supervisor: 'supervisor',
+  admin: 'admin',
+};
+
+module.exports = Roles;
